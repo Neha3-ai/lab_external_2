cc02e29f4a2f894a736b4552248b55be786211e7
Neha3-ai <mahankalineha3@gmail.com>
2025-11-21 10:51:58 +0530

    committed

---
+++ jenkinsjava.java
@@ -0,0 +1,10 @@
+public class jenkinsjava {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");        
+        int a = 5;
+        int b = 3;
+	int sum;
+	sum=a+b;
+        System.out.println("Sum of "+a+" and "+b+"is:"+sum);
+    }
+}
